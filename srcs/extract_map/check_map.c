3471a5dcd9ae131e6d8890b2f934652a32965d6e
zyux8 <hakerole@gmail.com>
2025-12-05 18:02:01 +0100

    to schoooooooollll
    Commit date: 2025-12-05 18:02

--- srcs/extract_map/check_map.c
+++ srcs/extract_map/check_map.c
@@ -6,22 +6,39 @@
 /*   By: ohaker <ohaker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 18:10:49 by ohaker            #+#    #+#             */
-/*   Updated: 2025/12/02 22:32:10 by ohaker           ###   ########.fr       */
+/*   Updated: 2025/12/05 17:59:47 by ohaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void init_map(t_data *data)
+void	init_map(t_data *data)
 {
-	t_map *map;
+	t_map	*map;
 
 	map = malloc(sizeof(t_map));
 	if (!map)
-		return (NULL); //exit function
+		return (NULL); // exit function
 	map->map = NULL;
 	map->player_facing = NULL;
 	data->map = map;
+}
+
+char	*get_single_text(char **lines, char *sig)
+{
+	int		x;
+	char	*line;
+	int		y;
+	
+	x = 0;
+	while (ft_strnstr(lines[x], sig, ft_strlen(lines[x])) == NULL)
+	x++;
+	line = ft_strnstr(lines[x], sig, ft_strlen(lines[x]));
+	y = 0;
+	
+	while (ft_isspace(line[y]))
+		y++;
+	
 }
 
 int	extract_texture(t_data *data, char **lines)
@@ -30,7 +47,6 @@ int	extract_texture(t_data *data, char **lines)
 
 	x = 0;
 	init_map(&data);
-	
 }
 
 int	check_map(char *filename, t_data *data)
