2a90d1b333f4f45a633f7aaaf48a8c7985b715d9
Pascal Bartholomaus <pbarthol@c2r9s4.42wolfsburg.de>
2026-01-13 22:34:52 +0100

    all
    Commit date: 2026-01-13 22:34

--- srcs/raycasting/cub3d_raycast_main.c
+++ srcs/raycasting/cub3d_raycast_main.c
@@ -6,17 +6,58 @@
 /*   By: pbarthol <pbarthol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 20:42:20 by pbarthol          #+#    #+#             */
-/*   Updated: 2026/01/13 19:23:35 by pbarthol         ###   ########.fr       */
+/*   Updated: 2026/01/13 22:32:41 by pbarthol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+/**
+ * Calculates a distance-based shading effect for a given RGB color.
+ * Colors appear darker the farther they are from the viewer.
+ *
+ * @param color - 24-bit RGB color encoded in a single unsigned int
+ * @param dist  - distance value used to derive the shading factor
+ * @return the shaded RGB color encoded as a 24-bit unsigned int
+ *
+ * The input color is decomposed into its red, green, and blue channels using
+ * bit shifts:
+ *   red   = 0xXX0000
+ *   green = 0x00XX00
+ *   blue  = 0x0000XX
+ *
+ * A shading factor is computed as 1 / dist. Because values greater than 1 would
+ * brighten the color instead of darkening it, the factor is limited to 1. Each
+ * channel is multiplied by the final factor to reduce intensity while
+ * preserving the color balance. The darkened channels are then recombined into
+ * a single 24-bit RGB color value and returned.
+ */
+
+unsigned int	c3_rycst_shade(unsigned int color, float dist)
+{
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+	float			shade;
+
+	r = (color >> 16) & 0xFF;
+	g = (color >> 8) & 0xFF;
+	b = color & 0xFF;
+
+	shade = 1 / dist;
+	if (shade > 1)
+		shade = 1;
+	r = (unsigned char)(r * shade);
+	g = (unsigned char)(g * shade);
+	b = (unsigned char)(b * shade);
+
+	return ((r << 16) | (g << 8) | b);
+}
+
 void	draw_line(t_data *data, int pixel_col, float dist)
 {
 	int	i;
 	int	line_height;
-	int	start;
 
 	if (dist < 1)
 		line_height = WIN_HEIGHT;
@@ -24,36 +65,23 @@ void	draw_line(t_data *data, int pixel_col, float dist)
 		line_height = WIN_HEIGHT / dist;
 	else
 		line_height = (WIN_HEIGHT / 2) / dist;
-	start = (WIN_HEIGHT / 2) - (line_height / 2);
 	i = 0;
-	while (i < start)
+	while (i < WIN_HEIGHT / 2)
 	{
-		if ((pixel_col > 20 && pixel_col < 20 + MINIMAP_W) && (i > 20 && i < 20 + MINIMAP_H))
-		{
-			i++;
-			continue ;
-		}
-		mlx_pixel_put(data->mlx, data->win, pixel_col, i, data->map->ceiling_color);
-		i++;
-	}
-	while (i < line_height)
-	{
-		if ((pixel_col > 20 && pixel_col < 20 + MINIMAP_W) && (i > 20 && i < 20 + MINIMAP_H))
-		{
-			i++;
-			continue ;
-		}
-		mlx_pixel_put(data->mlx, data->win, pixel_col, i, 0xFFFFFF / dist);
+		if (!(pixel_col > 20 && pixel_col < 20 + MINIMAP_W) || !(i > 20 && i < 20 + MINIMAP_H))
+			mlx_pixel_put(data->mlx, data->win, pixel_col, i, data->map->ceiling_color);
 		i++;
 	}
 	while (i < WIN_HEIGHT)
 	{
-		if ((pixel_col > 20 && pixel_col < 20 + MINIMAP_W) && (i > 20 && i < 20 + MINIMAP_H))
-		{
-			i++;
-			continue ;
-		}
 		mlx_pixel_put(data->mlx, data->win, pixel_col, i, data->map->floor_color);
+		i++;
+	}
+	i = (WIN_HEIGHT / 2) - (line_height / 2);
+	while (i < line_height)
+	{
+		if (!(pixel_col > 20 && pixel_col < 20 + MINIMAP_W) || !(i > 20 && i < 20 + MINIMAP_H))
+			mlx_pixel_put(data->mlx, data->win, pixel_col, i, c3_rycst_shade(create_rgb(20, 20, 200), dist));
 		i++;
 	}
 }
@@ -123,7 +151,7 @@ void	c3_rycst_main(t_data *data)
 				ray_pos_y += step_y;
 				dist_y += delta_dist_y;
 			}
-			if (data->map->map[ray_pos_y][ray_pos_x] != 0)
+			if (data->map->map[ray_pos_y][ray_pos_x] == 1 || (data->map->map[ray_pos_y][ray_pos_x] == DOOR && !player_close_to_door(data)))
 			{
 				angle_a = fabs(ray_angle - data->player->facing);
 				if (angle_a > data->actual_fov / 2)
