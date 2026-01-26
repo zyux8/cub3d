2488ae6200bf4027b8d6c8fd7354e85798c60655
zyux8 <hakerole@gmail.com>
2026-01-26 14:13:28 +0100

    save
    Commit date: 2026-01-26 14:13

--- bonus/cub3d.h
+++ bonus/cub3d.h
@@ -6,18 +6,19 @@
 /*   By: ohaker <ohaker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 22:05:36 by ohaker            #+#    #+#             */
-/*   Updated: 2026/01/22 22:14:37 by ohaker           ###   ########.fr       */
+/*   Updated: 2026/01/23 23:27:47 by ohaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../srcs/frees/free.h"
-# include "../srcs/handle_input/input.h"
-# include "../srcs/raycasting/cub3d_raycast.h"
-# include "libft/libft.h"
-# include "minilibx-linux/mlx.h"
+# include "srcs_bonus/draw_minimap/draw_minimap.h"
+# include "srcs_bonus/frees/free.h"
+# include "srcs_bonus/handle_input/input.h"
+# include "srcs_bonus/raycasting/cub3d_raycast.h"
+# include "../includes/libft/libft.h"
+# include "../includes/minilibx-linux/mlx.h"
 # include "structs.h"
 # include <errno.h>
 # include <fcntl.h>
@@ -48,6 +49,7 @@
 # define KEY_S 115
 # define KEY_D 100
 # define KEY_SP 32
+# define KEY_Z 122
 # define MOUSE_SENSITIVITY 0.0005
 # define MOVE_SPEED 0.12
 # define ROT_SPEED 0.07
@@ -57,6 +59,7 @@
 int			handle_destroy(t_data *data);
 int			key_press(int keycode, t_data *data);
 int			key_release(int keycode, t_data *data);
+int			mouse_move(int x, int y, t_data *data);
 
 // srcs/main.c
 void		init_data(t_data *data);
@@ -70,7 +73,17 @@ void		update_player_pos(t_data *data);
 void		move_player(t_data *data, double move_x, double move_y);
 
 // overlay/overlay_cigar.c
+t_cigar		*init_cigar(t_data *data);
 int			get_pixel_color(t_img *img, int x, int y);
+void		overlay_image(t_img *dest, t_img *src, int x_off, int y_off);
+int			load_cigar(t_data *data);
+
+// overlay/overlay_cigar.c
+void		loop_smoke(t_data *data);
+int			init_smoke(t_data *data, t_cigar *cigar);
+
+t_fps_count	*init_fps_count(void);
+void		c3_fps_count(t_data *data);
 void		update_player_movement(t_data *data, float speed);
 
 #endif
